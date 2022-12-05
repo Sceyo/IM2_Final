e2c5acef82ac04418a508cf1a15249578874b593
Josh Arkane Orbiso <orbisojo@gmail.com>
2022-12-05 14:59:07 +0800

    config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tugonon_construction');
+
+$mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+?>
